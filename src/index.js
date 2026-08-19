fa841fa01e003d9af0cf305f8d69aa73a7c89c3c
Frazer Smith <frazer.dev@icloud.com>
2026-08-19 07:20:06 +0100

    feat(lib): bump poppler from 25.11.0 to 26.02.0 (#73)
    * feat(lib): bump poppler from 25.11.0 to 26.02.0
    
    * refactor(lib): make git diffs easier

--- src/index.js
+++ src/index.js
@@ -10,12 +10,6 @@ const { resolve } = require("node:path");
 /**
  * The absolute path to the directory containing the Poppler binaries.
  */
-const binaryPath = resolve(
-	__dirname,
-	"lib",
-	"poppler-25.11.0",
-	"Library",
-	"bin"
-);
+const binaryPath = resolve(__dirname, "lib", "poppler", "Library", "bin");
 
 module.exports = binaryPath;
